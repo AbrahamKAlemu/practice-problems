09662988890a9c18bfcd0c7597693901f439a619
Abraham Alemu <abraham1123alemu@gmail.com>
2026-06-15 23:06:01 -0700

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,9 @@
+import bisect
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            idx = bisect.bisect_left(numbers, target - numbers[i], i + 1)
+            if idx != len(numbers) and numbers[idx] + numbers[i] == target:
+                return [i + 1, idx + 1]
+        return []
