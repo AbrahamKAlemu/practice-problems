71dd10ffbd0546e4308116df5297187d86e4633c
Abraham Alemu <abraham1123alemu@gmail.com>
2026-06-30 21:25:04 -0700

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(x):
+            if i * i == x:
+                return i
+            if i * i > x:
+                return i - 1
+        return x
