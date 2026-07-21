226402473f666ddacd99c01020ca3bf243294b1b
Abraham Alemu <abraham1123alemu@gmail.com>
2026-07-20 21:58:31 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        res = 0
+        for r in range(1, len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            if prices[r] - prices[l] > res:
+                res = prices[r] - prices[l]
+        return res
+        
